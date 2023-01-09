a35d61efc52ca19624ba0aa3032d76881a75ef01
João Vitor <jvitornas95@gmail.com>
2023-01-09 20:58:46 -0300

    move SUT dependencies creation to beforeAll

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -17,15 +17,19 @@ describe('FacebookAuthenticationService', () => {
   let crypto: MockProxy<TokenGenerator>
   let userAccountRepo: MockProxy<LoadUserAccountRepository & SaveFacebookAccountRepository>
   let sut: FacebookAuthenticationService
-  const token = 'any_token'
+  let token: string
 
-  beforeEach(() => {
+  beforeAll(() => {
+    token = 'any_token'
     facebookApi = mock()
     facebookApi.loadUser.mockResolvedValue({
       name: 'any_fb_name',
       email: 'any_fb_email',
       facebookId: 'any_fb_id'
     })
+  })
+
+  beforeEach(() => {
     userAccountRepo = mock()
     userAccountRepo.load.mockResolvedValue(undefined)
     userAccountRepo.saveWithFacebook.mockResolvedValueOnce({ id: 'any_acount_id' })
@@ -109,3 +113,6 @@ describe('FacebookAuthenticationService', () => {
     await expect(promise).rejects.toThrow(new Error('token_error'))
   })
 })
+
+// mdelo de negocios, camada domain
+// data
