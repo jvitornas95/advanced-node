5045433e852721ee56036d1be52c34334d1e1c9c
João Vitor <jvitornas@gmail.com>
2023-01-11 16:41:52 -0300

    ensure FacebookApi gets debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,35 +1,50 @@
+/* eslint-disable @typescript-eslint/semi */
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable @typescript-eslint/quotes */
+import { HttpGetClient } from "@/infra/http/";
+import { FacebookApi } from "@/infra/apis/";
 
-import { HttpGetClient } from '@/infra/http/'
-import { FacebookApi } from '@/infra/apis/'
+import { mock, MockProxy } from "jest-mock-extended";
 
-import { mock, MockProxy } from 'jest-mock-extended'
-
-describe('FacebookApi', () => {
-  let clientId: string
-  let clientSecret: string
-  let httpClient: MockProxy<HttpGetClient>
-  let sut: FacebookApi
+describe("FacebookApi", () => {
+  let clientId: string;
+  let clientSecret: string;
+  let httpClient: MockProxy<HttpGetClient>;
+  let sut: FacebookApi;
 
   beforeAll(() => {
-    clientId = 'any_client_id'
-    clientSecret = 'any_client_secret'
-    httpClient = mock<HttpGetClient>()
-  })
+    clientId = "any_client_id";
+    clientSecret = "any_client_secret";
+    httpClient = mock<HttpGetClient>();
+  });
 
   beforeEach(() => {
-    sut = new FacebookApi(httpClient, clientId, clientSecret)
-  })
+    httpClient.get.mockResolvedValueOnce({ access_token: "any_app_token" })
+    sut = new FacebookApi(httpClient, clientId, clientSecret);
+  });
 
-  it('should get app token', async () => {
-    await sut.loadUser({ token: 'any_client_token' })
+  it("should get app token", async () => {
+    await sut.loadUser({ token: "any_client_token" });
 
     expect(httpClient.get).toHaveBeenCalledWith({
-      url: 'https://graph.facebook.com/oauth/access_token',
+      url: "https://graph.facebook.com/oauth/access_token",
       params: {
         client_id: clientId,
         client_secret: clientSecret,
-        grant_type: 'client_credentials'
-      }
-    })
-  })
-})
+        grant_type: "client_credentials",
+      },
+    });
+  });
+
+  it("should get debug token", async () => {
+    await sut.loadUser({ token: "any_client_token" });
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: "https://graph.facebook.com/debug_token",
+      params: {
+        access_token: "any_app_token",
+        input_token: "any_client_token",
+      },
+    });
+  });
+});
