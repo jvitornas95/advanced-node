fb99f5b52a9b27b295617c10fec0091c6b9ce7ef
João Vitor <jvitornas@gmail.com>
2023-01-26 17:33:09 -0300

    configure express server

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,7 @@
+import './config/module-alias'
+import { app } from '@/main/config/app'
+import { env } from '@/main/config/env'
+
 import 'reflect-metadata'
+
+app.listen(env.appPort, () => console.log(`Server running at http://localhos:${env.appPort}`))
